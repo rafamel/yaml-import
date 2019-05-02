20183d8f08cfe51d578f9bab1c6400ef199f7bd7
Rafa Mel <melrcm@gmx.net>
2019-05-02 12:30:22 +0200

    feat: exports types from entry

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-/**
- * @module YIMP
- */
-
 export { default as write } from './write';
 export { default as read } from './read';
 export { default as getSchema } from './get-schema';
+export * from './types';
